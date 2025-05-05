64aaa94b3cffc7b141a3efea451883661d792c8e
milanmurali <milanmurali60@gmail.com>
2025-05-05 14:52:51 +0530

    README Creation

---
+++ Frontend/civiceye/src/main.jsx
@@ -0,0 +1,42 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import './index.css'
+// import App from './App.jsx'
+import { CELogin } from './CE/CELogin.jsx'
+import { CESignup } from './CE/CESignup.jsx'
+import { CEGuestHomePage } from './CE/CEGuestHomePage.jsx'
+import { CEUserHomePage } from './CE/CEUserHomePage.jsx'
+import { CEUserProfile } from './CE/CEUserProfile.jsx'
+import CEMyComplaints from './CE/CEMyComplaints.jsx'
+import { CEAdminDashboard } from './CE/CEAdminDashboard.jsx'
+import { CEAdminUserManagement } from './CE/CEAdminUserManagement.jsx'
+import { CEAdminComplaints } from './CE/CEAdminComplaints.jsx'
+import { CEAdminFeedback } from './CE/CEAdminFeedback.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+
+    <BrowserRouter>
+ 
+      <Routes>
+        <Route path="/" element={<Navigate to="/landing" />} />   // Redirect to landing page
+        <Route path='/landing' element={<CEGuestHomePage />} />
+        <Route path='/home' element={<CEUserHomePage />} />
+        <Route path="/signin" element={<CELogin />} />
+        <Route path="/signup" element={<CESignup />} />
+        <Route path='/myprofile' element={<CEUserProfile />} />
+        <Route path='/mycomplaints' element={<CEMyComplaints />} />
+
+        <Route path='/dashboard' element={<CEAdminDashboard />} />
+        <Route path='/userlist' element={<CEAdminUserManagement />} />
+        <Route path='/complaints' element={<CEAdminComplaints />} />
+        <Route path='/feedback' element={<CEAdminFeedback />} />
+      </Routes>
+ 
+    </BrowserRouter>
+
+    {/* <App /> */}
+  </StrictMode>
+
+  ,)
